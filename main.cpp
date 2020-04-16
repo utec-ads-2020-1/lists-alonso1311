130dd8f17ff37efb6fe9c68594556fb7ffad0267
alonso1311 <alonso.barrios@utec.edu.pe>
2020-04-15 21:12:38 -0500

    finish queue

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include"structures/header.h"
 //#include "test/tester.h"
 #include"structures/stack.h"
-
-using namespace std;
+#include"structures/queue.h"
 
 int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl;
@@ -28,6 +27,24 @@ int main(int argc, char *argv[]) {
 
     delete stack1;
     */ 
-    
+
+    /*
+    //-QUEUE test
+    auto* queue1 = new queue<int>(3);
+    queue1->push(4);
+    queue1->push(6);
+    queue1->push(9);
+    queue1->push(1);
+    queue1->pop();
+    queue1->push(10);
+    queue1->push(6);
+    queue1->print();
+    cout << "Front: " << queue1->front() << endl;
+    cout << "Back: " << queue1->back() << endl;
+    cout << "Size: " << queue1->size() << endl;
+
+    delete queue1;
+    */
+
     return EXIT_SUCCESS;
 }
--- structures/queue.h
+++ structures/queue.h
@@ -1,9 +1,7 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#include<iostream>
-
-using namespace std;
+#include"header.h"
 
 #define MAX 1000 
 
@@ -25,48 +23,84 @@ public:
 
 	int size();
 	bool empty();
+
+    //-Extra functions
+    void reserve();
+    void print();
 };
 
 //--------------------------------------------------------------------
 template<typename T>
 queue<T>::queue(int size){
-    cout << size << endl;
+    capacity = size;
+    top = 0;
+    data = new T[capacity];
 }
 
 template<typename T>
 queue<T>::~queue(){
-    
+    delete[] data; 
 }
 
 //--------------------------------------------------------------------
 template<typename T>
 void queue<T>::push(T item){
+    if(top == capacity)
+        reserve();
 
+    data[top] = item;
+    top++;
 }
 template<typename T>
 void queue<T>::pop(){
+    if(empty() == true)
+        cerr << "The queue is empty\n";
+    else{
+        for(int i = 0; i < top; ++i)
+            data[i] = data[i+1];
 
+        top--;
+    }
 }
 
 template<typename T>
 T queue<T>::front(){
-
+    return data[0];
 }
 
 template<typename T>
 T queue<T>::back(){
-
+    return data[top-1];
 }
 
 //--------------------------------------------------------------------
 template<typename T>
 int queue<T>::size(){
-
+    return top;
 }
 
 template<typename T>
 bool queue<T>::empty(){
+    return (top == 0) ? true : false;
+}
 
+//----------------EXTRA FUNCTIONS------------------------------------
+template<typename T>
+void queue<T>::reserve(){
+    capacity += MAX;
+    T* temp_data = new T[capacity];
+
+    for(int i = 0; i < top; ++i)
+        temp_data[i] = data[i];
+
+    delete[] data;
+    data = temp_data;
+}
+
+template<typename T>
+void queue<T>::print(){
+    for(int i = 0; i < top; ++i)
+        cout << data[i] << endl;
 }
 
 #endif
