43fd0a351bd44fb66c15868c04553f0a28102739
alonso1311 <alonso.barrios@utec.edu.pe>
2020-04-16 19:02:40 -0500

    fix resize in queue and stack

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 
     //Tester::execute();
     
-        
+    /*    
     //-STACK test
     auto* stack1 = new stack<int>(3);
     stack1->push(3);
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
     cout << "Size: " << stack1->size() << endl;
 
     delete stack1;
-     
+    */ 
 
     /*
     //-QUEUE test
